dc44c2e8fbdf21606a588e9846878377427b8c2a
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2025-08-24 10:29:47 -0700

    Change to PG, run application

--- backend/WebServer/Program.cs
+++ backend/WebServer/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -25,10 +25,10 @@ else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseRouting();
 
---
+++ backend/WebServer/README.md
@@ -0,0 +1,7 @@
+# Running Locally
+Install the .NET SDK 9
+
+`dotnet tool install --global dotnet-ef`
+`dotnet ef database update`
+
+Run the server by starting debug in VsCode.
