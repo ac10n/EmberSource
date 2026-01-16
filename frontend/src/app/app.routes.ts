1546a2df0b227146a7c65df96196f2f3fe55c33d
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2026-01-15 22:16:49 -0800

    Create initial pages

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { CreateAccountComponent } from './create-account/create-account';
+import { LoginComponent } from './login/login';
+import { DashboardComponent } from './dashboard/dashboard';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'create-account', component: CreateAccountComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', component: DashboardComponent },    
+];
