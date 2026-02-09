0f05a0829d30b22879d1e58c889651645ae65d6d
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2026-02-08 21:04:16 -0800

    remove spec files

--- frontend/EmberUI/src/app/services/markdownit-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MarkdownitPipe } from './markdownit-pipe';
-
-describe('MarkdownitPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MarkdownitPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
